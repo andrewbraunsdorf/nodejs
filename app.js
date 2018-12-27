a3c58dea4c958bbff393a6ecb51a0bbf81a312d3
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2018-12-27 22:43:34 +0000

    added dirname, filename, SetInterval, and setTimeout to app.js

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
-var fs = require('fs');
+console.log("hey ninjas");
 
-var file = fs.readFile('input.txt','utf8',function(err,data){
-    fs.writeFile('writeme.txt',data);
-    console.log(data);
-});
+console.log(__dirname);
+console.log(__filename);
 
-console.log('This is an instruction outside the sync file system.');
+var time = 0;
+var timer = setInterval(function(){
+    time += 2;
+    console.log(time + " seconds have passed");
+    if ( time > 5){
+        clearInterval(timer);
+    }
+}, 2000);
+
+
+// setTimeout(function(){
+//     console.log("3 seconds have passed")
+// }, 3000);
+
+
+// var fs = require('fs');
+
+// var file = fs.readFile('input.txt','utf8',function(err,data){
+//     fs.writeFile('writeme.txt',data);
+//     console.log(data);
+// });
+
+// console.log('This is an instruction outside the sync file system.');
