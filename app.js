708959f8d53890425d5e55bab0edaddab01a8f01
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2018-12-27 22:19:27 +0000

    Learned about V8 node.js

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
-console.log("hey ninjas");
+var fs = require('fs');
 
+var file = fs.readFile('input.txt','utf8',function(err,data){
+    fs.writeFile('writeme.txt',data);
+    console.log(data);
+});
 
-// var fs = require('fs');
-
-// var file = fs.readFile('input.txt','utf8',function(err,data){
-//     fs.writeFile('writeme.txt',data);
-//     console.log(data);
-// });
-
-// console.log('This is an instruction outside the sync file system.');
+console.log('This is an instruction outside the sync file system.');
