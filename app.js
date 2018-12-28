889e65ba71ae783810e32c852b1af8123f31328f
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2018-12-28 01:43:46 +0000

    learned different ways to link multiple JS pages together using module.exports, singularly, together, and in an object.

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-var counter = require("./count");
+var stuff = require("./stuff");
 
-console.log(counter(["shaun", "crystal", "ryu"]));
+console.log(stuff.counter(["shaun", "crystal", "ryu"]));
+console.log(stuff.adder(5,6));
+console.log(stuff.adder(stuff.pi,5));
 
 // function callFunction(fun){
 //    fun();
