086085ee5ab5432a05cdc0efa538b28ae1a0458d
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-01-14 01:56:42 +0000

    Readable Streams, and using method .createReadStream() to send chunks of data

--- app.js
+++ app.js
@@ -1,72 +1,89 @@
 var http = require("http");
+var fs = require("fs");
 
-var server = http.createServer(function(req, res){
-	console.log("request was made: " + req.url);
-	res.writeHead(200, {"Content-Type": "text/plain"});
-	res.end("Hey Ninjas!");
+var myReadStream = fs.createReadStream(__dirname + "/clientsandservers.txt", "utf8");
+
+myReadStream.on("data", function(chunk){
+	console.log("new chunk received");
+	console.log(chunk);
 });
 
-server.listen(8080, "0.0.0.0");
-console.log("You are listening to port 8080");
+
+
+// var server = http.createServer(function(req, res){
+// 	console.log("request was made: " + req.url);
+// 	res.writeHead(200, {"Content-Type": "text/plain"});
+// 	res.end("Hey Ninjas!");
+// });
+
+// server.listen(8080, "0.0.0.0");
+// console.log("You are listening to port 8080");
 
 
 
-// var fs = require("fs");
 
-// fs.unlink("./stuff/writeMe.txt", function(){
+
+
+
+
+
+
+// // var fs = require("fs");
+
+// // fs.unlink("./stuff/writeMe.txt", function(){
 	
 
-// fs.rmdir("stuff");
-// });
-// // fs.mkdir("stuff", function(){
-// // 	fs.readFile("readMe.text", "utf8", function(err, data){
-// // 	fs.writeFile("./stuff/writeMe.txt", data);
-// // 	});
+// // fs.rmdir("stuff");
 // // });
+// // // fs.mkdir("stuff", function(){
+// // // 	fs.readFile("readMe.text", "utf8", function(err, data){
+// // // 	fs.writeFile("./stuff/writeMe.txt", data);
+// // // 	});
+// // // });
 
 
-// // fs.unlink("writeMe.txt");
+// // // fs.unlink("writeMe.txt");
 
 
-// //delete files using unlink, 2nd time running it, there is an error, as no such file exists, then used .mkdirSync("stuff") to create a directory called stuff, to remove a directory you use .rmdirSync("stuff"), then created asynchronous version by using .mkdir("stuff"), then deleted it using .rmdir("stuff"), but can only remove the directory when it is empty. had to use the unlink method to remove the file first."
-// // fs.readFile("readme.txt", "utf8", function(err, data){
-// // 	fs.writeFile("writeMe.txt", data);
-// // });
-// //code
-// //console.log("test");
-// // var readMe = fs.readFileSync("readme.txt", "utf8");
-// //fs.writeFileSync("writeMe.txt", readMe);
+// // //delete files using unlink, 2nd time running it, there is an error, as no such file exists, then used .mkdirSync("stuff") to create a directory called stuff, to remove a directory you use .rmdirSync("stuff"), then created asynchronous version by using .mkdir("stuff"), then deleted it using .rmdir("stuff"), but can only remove the directory when it is empty. had to use the unlink method to remove the file first."
+// // // fs.readFile("readme.txt", "utf8", function(err, data){
+// // // 	fs.writeFile("writeMe.txt", data);
+// // // });
+// // //code
+// // //console.log("test");
+// // // var readMe = fs.readFileSync("readme.txt", "utf8");
+// // //fs.writeFileSync("writeMe.txt", readMe);
 
-// // var events = require("events");
+// // // var events = require("events");
 
-// // var util = require('util');
+// // // var util = require('util');
 
-// // var Person = function(name){
-// // 	this.name = name;
-// // };
+// // // var Person = function(name){
+// // // 	this.name = name;
+// // // };
 
-// // util.inherits(Person, events.EventEmitter);
+// // // util.inherits(Person, events.EventEmitter);
 
-// // var james = new Person("James");
-// // var mary = new Person("Mary");
-// // var ryu = new Person("ryu");
+// // // var james = new Person("James");
+// // // var mary = new Person("Mary");
+// // // var ryu = new Person("ryu");
 
-// // var people = [james, mary, ryu];
+// // // var people = [james, mary, ryu];
 
-// // people.forEach(function(person){
-// // 	person.on("speak", function(msg){
-// // 		console.log(person.name + " said: " + msg);
-// // 	});
-// // });
+// // // people.forEach(function(person){
+// // // 	person.on("speak", function(msg){
+// // // 		console.log(person.name + " said: " + msg);
+// // // 	});
+// // // });
 
-// // james.emit("speak", "hey dudes");
-// // ryu.emit("speak", "I want a curry");
+// // // james.emit("speak", "hey dudes");
+// // // ryu.emit("speak", "I want a curry");
 
 
-// // var myEmitter = new events.EventEmitter();
+// // // var myEmitter = new events.EventEmitter();
 
-// // myEmitter.on("someEvent", function(msg){
-// // 	console.log(msg);
-// // });
+// // // myEmitter.on("someEvent", function(msg){
+// // // 	console.log(msg);
+// // // });
 
-// // myEmitter.emit("someEvent", "the event was emitted");
+// // // myEmitter.emit("someEvent", "the event was emitted");
