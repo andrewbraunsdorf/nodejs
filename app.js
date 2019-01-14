073d57660409252f0f92ea7e94c67ea11eb8af08
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-01-14 23:25:52 +0000

    added pipes to the server using myReadstream.pipe() method

--- app.js
+++ app.js
@@ -1,32 +1,30 @@
 var http = require("http");
 var fs = require("fs");
 
-var myReadStream = fs.createReadStream(__dirname + "/clientsandservers.txt", "utf8");
-var myWriteStream = fs.createWriteStream(__dirname + "/writeMe.txt");
 
-myReadStream.on("data", function(chunk){
-	console.log("new chunk received");
-	console.log(chunk);
-	myWriteStream.write(chunk);
+
+
+
+var server = http.createServer(function(req, res){
+	console.log("request was made: " + req.url);
+	res.writeHead(200, {"Content-Type": "text/plain"});
+	var myReadStream = fs.createReadStream(__dirname + "/clientsandservers.txt", "utf8");
+	myReadStream.pipe(res);
 });
 
+server.listen(8080, "0.0.0.0");
+console.log("You are listening to port 8080");
 
 
-// var server = http.createServer(function(req, res){
-// 	console.log("request was made: " + req.url);
-// 	res.writeHead(200, {"Content-Type": "text/plain"});
-// 	res.end("Hey Ninjas!");
+
+
+
+
+// myReadStream.on("data", function(chunk){
+// 	console.log("new chunk received");
+// 	console.log(chunk);
+// 	myWriteStream.write(chunk);
 // });
-
-// server.listen(8080, "0.0.0.0");
-// console.log("You are listening to port 8080");
-
-
-
-
-
-
-
 
 
 
