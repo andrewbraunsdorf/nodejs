efa7d803be28854f803799667223a609e5ccc5e1
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-01-17 00:27:38 +0000

    created basic skeleton of our todo app

--- app.js
+++ app.js
@@ -1,32 +1,20 @@
 var express = require("express");
-var bodyParser = require("body-parser");
-var app = express();
+var todocontroller = require("./controllers/todcontroller");
+var app =express();
 
 
-var urlencodedParser = bodyParser.urlencoded({ extended: false });
-
+//set up template engine
 app.set("view engine", "ejs");
-app.use("/assets", express.static("stuff"));
 
-app.get("/", function(req, res){
-	res.render("index");
-});
+//static files
+app.use(express.static("./public"));
 
-app.get("/contact", function(req, res){
-	res.render("contact", {qs: req.query});
-});
+//fire controllers
+todocontroller(app);
 
-app.post("/contact", urlencodedParser, function(req, res){
-	console.log(req.body);
-	res.render("contact-success", {data: req.body});
-});
-
-app.get("/profile/:name", function(req, res) {
-	var data = {age: 29, job: "ninja", hobbies: ["eating", "fighting", "fishing"]};
-	res.render("profile", {person: req.params.name, data: data});
-});
-
+//listen to port
 app.listen(8080);
+console.log("You are listening to port 8080");
 
 
 
@@ -53,6 +41,36 @@ app.listen(8080);
 
 
 
+
+// var express = require("express");
+// var bodyParser = require("body-parser");
+// var app = express();
+
+
+// var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+// app.set("view engine", "ejs");
+// app.use("/assets", express.static("stuff"));
+
+// app.get("/", function(req, res){
+// 	res.render("index");
+// });
+
+// app.get("/contact", function(req, res){
+// 	res.render("contact", {qs: req.query});
+// });
+
+// app.post("/contact", urlencodedParser, function(req, res){
+// 	console.log(req.body);
+// 	res.render("contact-success", {data: req.body});
+// });
+
+// app.get("/profile/:name", function(req, res) {
+// 	var data = {age: 29, job: "ninja", hobbies: ["eating", "fighting", "fishing"]};
+// 	res.render("profile", {person: req.params.name, data: data});
+// });
+
+// app.listen(8080);
 
 
 // var http = require("http");
