5efe6bea4179b8adf72674ad6e3d80e507697ddb
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-01-17 01:44:04 +0000

    installed mongoose, added a database, used mongoose to connect to database, created a schema, created new model, then created a new item, and saved that to the database

--- controllers/todocontroller.js
+++ controllers/todocontroller.js
@@ -1,4 +1,19 @@
 var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+
+//connect to the database added , {useNewUrlParser: true}
+mongoose.connect("mongodb://test:tester1@ds259154.mlab.com:59154/todoapplication", {useNewUrlParser: true});
+
+//Create a schema - this is like a bluepring
+var todoSchema = new mongoose.Schema({
+	item: String	
+});
+
+var Todo = mongoose.model("Todo", todoSchema);
+var itemOne =Todo({item: "buy flowers"}).save(function(err){
+	if (err) throw err;
+	console.log("item saved");
+});
 
 var data =[{item: "get milk"}, {item: "walk dog"}, {item: "kick some coding ass"}];
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
